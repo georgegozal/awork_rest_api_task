77c6024c93bf22890e74ad9d5d5d6b666c5a40d0
georgegozal <georgeg9@protonmail.com>
2023-01-12 16:43:26 +0400

    minor changes

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
@@ -7,6 +6,5 @@ from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
 ma = Marshmallow()
 jwt = JWTManager()
