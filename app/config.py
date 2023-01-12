5c04c432993403dd2f5932b40c46085206b78799
georgegozal <georgeg9@protonmail.com>
2023-01-12 11:41:29 +0400

    config object for app

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'asldkj29usdk 2'
