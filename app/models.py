5994217dcad6423d439c0c2784a1febb4c562f91
georgegozal <georgeg9@protonmail.com>
2023-01-15 16:31:03 +0400

    minor changes

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class User(db.Model, Base):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, index=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
 
